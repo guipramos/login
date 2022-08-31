09c24832cb77929bfead7bfbd37a96d91b350903
Guilherme Ramos <gui.pramos@hotmail.com>
2022-08-31 16:01:02 -0300

    Armazenando dados do usuario usando AsyncStorage

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,9 +1,12 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import * as auth from "../services/auth";
+import AsyncStorage from "@react-native-community/async-storage";
 
 interface AuthContextData {
   signed: boolean;
   user: object | null;
+  loading: boolean;
+  signIn(): Promise<void>;
   signOut(): void;
 }
 
@@ -11,18 +14,39 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [user, setUser] = useState<object | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadStorageData() {
+      const storagedUser = await AsyncStorage.getItem("@RNAuth:user");
+      const storagedToken = await AsyncStorage.getItem("@RNAuth:token");
+
+      if (storagedUser && storagedToken) {
+        setUser(JSON.parse(storagedUser));
+      }
+	  setLoading(false);
+    }
+
+    loadStorageData();
+  });
 
   async function signIn() {
     const response = await auth.signIn();
     setUser(response.user);
+
+    await AsyncStorage.setItem("@RNAuth:user", JSON.stringify(response.user));
+    await AsyncStorage.setItem("@RNAuth:token", response.token);
   }
 
   async function signOut() {
+    await AsyncStorage.clear();
     setUser(null);
   }
 
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ signed: !!user, user, loading, signIn, signOut }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,23 @@
-import React, {useContext} from 'react';
+import React, { useContext } from "react";
+import { View, ActivityIndicator } from "react-native";
 
-import AuthContext from '../contexts/auth';
+import AuthContext from "../contexts/auth";
 
-import AuthRoutes from '../routes/auth.routes';
-import AppRoutes from '../routes/app.routes';
+import AuthRoutes from "../routes/auth.routes";
+import AppRoutes from "../routes/app.routes";
 
 const Routes: React.FC = () => {
-    const { signed } = useContext(AuthContext);
-    return signed ? <AppRoutes /> : <AuthRoutes />    
+  const { signed, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#666" />
+      </View>
+    );
+  }
+
+  return signed ? <AppRoutes /> : <AuthRoutes />;
 };
 
 export default Routes;
